92a8ede1364eff396d2f0db2cbfa94d99e4a4d6d
AmberWow123 <eatingwow@gmail.com>
2019-08-20 15:54:29 +0800

    installed element ui

---
+++ src/components/Todo/Todo.vue
@@ -0,0 +1,47 @@
+<!--<template>
+    <div class="todo">
+        <h1>FHDATime</h1>
+        <div class="todoList" v-for="todoList in todo" v-bind:key="todoList.id">
+            {{todoList.title}}
+        </div>
+        <p>quarter</p>
+        
+        
+    </div>
+
+</template> -->
+
+<template>
+  <div>
+    <el-button type="success">测试</el-button>
+  </div>
+  <div class = "el-row" style = "margin-left: -6px; margin-right: -6px;">
+
+  </div>
+</template>
+
+<!--<script>
+export default {
+    name: "Todo",
+    data() {
+        return {
+            todo: [
+                {
+                    id: 1,
+                    title: "id:one",
+                    completed: false
+                },
+                {
+                    id: 2,
+                    title: "id:two",
+                    completed: true
+                }
+            ]
+        };
+    }
+};
+</script> -->
+
+<style>
+
+</style>
